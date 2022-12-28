75ffec9af056e787ccdcb505e4e84e4e6d0667d2
Eliedson <especialista1979@gmail.com>
2022-12-27 21:51:27 -0300

    Updates

---
+++ precache-manifest.fb8236ec55782e5bc2d196ff4af668c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345aa01839cb8ff863aed4d846c84235",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "f69f913a2c38686e656b",
+    "url": "/trivia/static/css/main.e72d4387.chunk.css"
+  },
+  {
+    "revision": "4dcb711175854313482f",
+    "url": "/trivia/static/js/2.5f9070d1.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia/static/js/2.5f9070d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f69f913a2c38686e656b",
+    "url": "/trivia/static/js/main.f1a670bf.chunk.js"
+  },
+  {
+    "revision": "13da866554f636ecdfac",
+    "url": "/trivia/static/js/runtime-main.62eca463.js"
+  }
+]);
